aed3f7a7649d2d44cc4779b891289acc06b593ed
Simon Manzler <simanzler@gmail.com>
2025-03-21 14:08:28 -0400

    added conditionals to start workout

--- app/(tabs)/start_workout.tsx
+++ app/(tabs)/start_workout.tsx
@@ -6,19 +6,84 @@ import {
   ScrollView,
   Animated,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import { router, useNavigation } from "expo-router";
 import { Entypo } from "@expo/vector-icons";
 import { useWorkout } from "@/providers/WorkoutProvider";
+import database, { workoutsCollection } from "@/db";
 
 export default function StartWorkoutScreen() {
   const navigation = useNavigation();
-  const { startWorkout } = useWorkout();
+  const { activeWorkoutId, startWorkout } = useWorkout();
   const scrollY = useRef(new Animated.Value(0)).current;
 
+  const handleDelete = async () => {
+    if (!activeWorkoutId) return;
+
+    try {
+      const workout = await workoutsCollection.find(activeWorkoutId);
+      const workoutExercises = await workout.workoutExercises.fetch();
+
+      database.write(async () => {
+        const setsToDelete = await Promise.all(
+          workoutExercises.map((we) => we.sets.fetch())
+        );
+
+        const workoutExerciseDeletions = await Promise.all(
+          workoutExercises.map((we) => we.markAsDeleted())
+        );
+        const setDeletions = await Promise.all(
+          setsToDelete.flat().map((set) => set.markAsDeleted())
+        );
+        const workoutDeletion = await workout.markAsDeleted();
+
+        const batchOps = [
+          ...workoutExerciseDeletions,
+          ...setDeletions,
+          workoutDeletion,
+        ];
+
+        await database.batch(...batchOps);
+      });
+    } catch (error) {
+      console.error("Failed to delete workout:", error);
+    }
+  };
+
   const start = () => {
-    startWorkout();
-    router.push("/(modals)/workout");
+    if (activeWorkoutId) {
+      Alert.alert(
+        "Active Workout",
+        "You already have an active workout. Starting a new workout will override the current one. Do you want to continue?",
+        [
+          {
+            text: "Cancel",
+            style: "cancel",
+          },
+          {
+            text: "Resume Workout",
+            onPress: () => {
+              router.push("/(modals)/workout");
+            },
+          },
+          {
+            text: "Start New Workout",
+            onPress: async () => {
+              await handleDelete();
+
+              await startWorkout();
+              router.push("/(modals)/workout");
+            },
+            style: "destructive",
+          },
+        ],
+        { cancelable: true }
+      );
+    } else {
+      startWorkout();
+      router.push("/(modals)/workout");
+    }
   };
 
   const textOpacity = scrollY.interpolate({
