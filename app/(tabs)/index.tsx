a0cbb7e6c6022b2fd6f3f262f5665501c812dfa0
Simon Manzler <simanzler@gmail.com>
2025-03-25 23:45:36 -0400

    add sync button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,14 +6,26 @@ import {
   ScrollView,
   Animated,
   SafeAreaView,
+  Button,
 } from "react-native";
 import { useAuth } from "@/providers/AuthProvider";
 import LoginScreen from "../(auth)/login";
 import { useTheme } from "@react-navigation/native";
+import { mySync } from "@/db/sync";
+import { useSync } from "@/providers/SyncProvider";
+import database from "@/db";
 
 export default function ProfileScreen() {
   const { user } = useAuth();
+  const { updateLastSync } = useSync();
   const theme = useTheme();
+
+  async function resetDatabase() {
+    await database.write(async () => {
+      await database.unsafeResetDatabase();
+      console.log("Database reset successfully!");
+    });
+  }
 
   if (!user) return <LoginScreen />;
 
@@ -26,6 +38,11 @@ export default function ProfileScreen() {
         <Text style={[styles.user, { color: theme.colors.text }]}>
           Logged in as {user.email}
         </Text>
+        <Button
+          title="Sync"
+          onPress={() => mySync(user, updateLastSync, () => {})}
+        />
+        {/* <Button title="Reset DB" onPress={resetDatabase} /> */}
       </ScrollView>
     </SafeAreaView>
   );
