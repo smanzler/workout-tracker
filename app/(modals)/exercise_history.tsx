d0482d7f0aa3cc5f4a7baefd6a50d3ee61937705
Simon Manzler <simanzler@gmail.com>
2025-04-09 23:27:01 -0400

    added prs and history

--- app/(modals)/exercise_history.tsx
+++ app/(modals)/exercise_history.tsx
@@ -6,6 +6,31 @@ import { WorkoutExercise } from "@/models/WorkoutExercise";
 import { exercisesCollection } from "@/db";
 import { Ionicons } from "@expo/vector-icons";
 import { useTheme } from "@react-navigation/native";
+import { BodyScrollView } from "@/components/BodyScrollViiew";
+import { ThemedText } from "@/components/ThemedText";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { getMaxPRBefore } from "@/utils/PRs";
+import dayjs from "dayjs";
+import ExerciseHistoryListItem from "@/components/ExerciseHistoryListItem";
+
+type PRs = {
+  weight: {
+    value: number;
+    date: number;
+  };
+  volume: {
+    value: number;
+    date: number;
+  };
+  oneRepMax: {
+    value: number;
+    date: number;
+  };
+};
 
 const ExerciseHistory = () => {
   const { exerciseId, title } = useLocalSearchParams<{
@@ -17,21 +42,47 @@ const ExerciseHistory = () => {
     WorkoutExercise[] | undefined
   >(undefined);
   const { colors } = useTheme();
+  const [loading, setLoading] = useState(true);
+  const [prs, setPrs] = useState<PRs>();
+
+  const opacity = useSharedValue(0);
 
   useEffect(() => {
     const load = async () => {
       if (!exerciseId) return;
+
+      setLoading(true);
+
       const e = await exercisesCollection.find(exerciseId);
       setExercise(e);
 
       const we = await e.workoutExercises.fetch();
       setWorkoutExercises(we);
+
+      if (we.length === 0) return;
+
+      const p = await getMaxPRBefore(new Date().getTime(), exerciseId);
+      setPrs(p);
+
+      setLoading(false);
     };
     load();
   }, [exerciseId]);
 
+  useEffect(() => {
+    if (!loading) {
+      opacity.value = withTiming(1, { duration: 500 });
+    }
+  }, [loading]);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
   return (
-    <View>
+    <BodyScrollView style={{ paddingHorizontal: 20 }} stickyHeaderIndices={[1]}>
       <Stack.Screen
         options={{
           headerTitle: title,
@@ -42,11 +93,154 @@ const ExerciseHistory = () => {
           ),
         }}
       />
-      <Text>ExerciseHistory</Text>
-    </View>
+
+      <Animated.View style={animatedStyle}>
+        {workoutExercises && workoutExercises?.length !== 0 ? (
+          <>
+            <ThemedText
+              style={{
+                fontSize: 20,
+                fontWeight: 600,
+                opacity: 0.6,
+                marginBottom: 40,
+              }}
+            >
+              {exercise?.muscleGroup}
+            </ThemedText>
+
+            <ThemedText type="subtitle" style={{ marginBottom: 10 }}>
+              Personal Records
+            </ThemedText>
+
+            <View style={styles.prContainer}>
+              <View style={[styles.prCard, { backgroundColor: colors.card }]}>
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prSubText}
+                >
+                  Max Weight
+                </ThemedText>
+
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prWeightText}
+                >
+                  {prs?.weight?.value} lbs
+                </ThemedText>
+
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prSubText}
+                >
+                  {dayjs(prs?.weight?.date).format("[on] MMM D")}
+                </ThemedText>
+              </View>
+
+              <View style={[styles.prCard, { backgroundColor: colors.card }]}>
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prSubText}
+                >
+                  One Rep Max
+                </ThemedText>
+
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prWeightText}
+                >
+                  {Math.floor(prs?.oneRepMax?.value ?? 0)} lbs
+                </ThemedText>
+
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prSubText}
+                >
+                  {dayjs(prs?.oneRepMax?.date).format("[on] MMM D")}
+                </ThemedText>
+              </View>
+
+              <View style={[styles.prCard, { backgroundColor: colors.card }]}>
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prSubText}
+                >
+                  Max Volume
+                </ThemedText>
+
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prWeightText}
+                >
+                  {prs?.volume?.value} lbs
+                </ThemedText>
+
+                <ThemedText
+                  adjustsFontSizeToFit={true}
+                  numberOfLines={1}
+                  style={styles.prSubText}
+                >
+                  {dayjs(prs?.volume?.date).format("[on] MMM D")}
+                </ThemedText>
+              </View>
+            </View>
+
+            <ThemedText type="subtitle" style={{ marginBottom: 10 }}>
+              History
+            </ThemedText>
+
+            {workoutExercises.map((workoutExercise) => (
+              <ExerciseHistoryListItem
+                key={workoutExercise.id}
+                workoutExercise={workoutExercise}
+              />
+            ))}
+          </>
+        ) : (
+          <View style={{ alignItems: "center", padding: 40, marginTop: 100 }}>
+            <Ionicons name="barbell" color={colors.text} size={50} />
+            <ThemedText type="subtitle">No History</ThemedText>
+            <ThemedText
+              type="defaultSemiBold"
+              style={{ opacity: 0.6, textAlign: "center" }}
+            >
+              Your PRs and Hisory will appear here when you use the exercise
+            </ThemedText>
+          </View>
+        )}
+      </Animated.View>
+    </BodyScrollView>
   );
 };
 
 export default ExerciseHistory;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  prContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 40,
+  },
+  prCard: {
+    padding: 12,
+    borderRadius: 12,
+    alignItems: "center",
+    width: "32%",
+  },
+  prSubText: {
+    fontSize: 16,
+    fontWeight: 600,
+    opacity: 0.6,
+  },
+  prWeightText: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+});
---
+++ components/ExerciseHistoryListItem.tsx
@@ -0,0 +1,102 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { WorkoutExercise } from "@/models/WorkoutExercise";
+import { Set } from "@/models/Set";
+import { withObservables } from "@nozbe/watermelondb/react";
+import { useTheme } from "@react-navigation/native";
+import { Workout } from "@/models/Workout";
+import { ThemedText } from "./ThemedText";
+import dayjs from "dayjs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const ExerciseHistoryListItem = ({
+  workoutExercise,
+  workout,
+  sets,
+}: {
+  workoutExercise: WorkoutExercise;
+  workout: Workout;
+  sets: Set[];
+}) => {
+  const { colors } = useTheme();
+
+  const countPRs = (sets: Set[]) => {
+    let prCount = 0;
+
+    sets.forEach((set) => {
+      if (set.isWeightPr) prCount++;
+      if (set.is1RMPr) prCount++;
+      if (set.isVolumePr) prCount++;
+    });
+
+    if (prCount === 1) {
+      return "1 PR";
+    } else {
+      return `${prCount} PRs`;
+    }
+  };
+
+  return (
+    <View style={[styles.card, { backgroundColor: colors.card }]}>
+      <ThemedText style={{ fontSize: 18, fontWeight: 600 }}>
+        {workout.name || "Workout"}
+      </ThemedText>
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginBottom: 5,
+        }}
+      >
+        <ThemedText style={{ fontSize: 16, fontWeight: 600, opacity: 0.6 }}>
+          {dayjs(workout.startTime).format("MMM D, YYYY")}
+        </ThemedText>
+
+        <View style={styles.textWithIcon}>
+          <MaterialCommunityIcons
+            style={{ opacity: 0.6 }}
+            name="trophy"
+            color={colors.text}
+            size={16}
+          />
+          <ThemedText style={{ fontSize: 16, fontWeight: 600, opacity: 0.6 }}>
+            {countPRs(sets)}
+          </ThemedText>
+        </View>
+      </View>
+
+      {sets.map((set) => (
+        <View style={[styles.textWithIcon]}>
+          <ThemedText style={{ fontWeight: 600 }} key={set.id}>
+            {set.weight} lbs × {set.reps}
+          </ThemedText>
+          <MaterialCommunityIcons name="trophy" color={"#e3c254"} size={16} />
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const enhance = withObservables(
+  ["workoutExercise"],
+  ({ workoutExercise }: { workoutExercise: WorkoutExercise }) => ({
+    workoutExercise,
+    workout: workoutExercise.workout,
+    sets: workoutExercise.sets,
+  })
+);
+
+export default enhance(ExerciseHistoryListItem);
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 12,
+    borderRadius: 12,
+  },
+  textWithIcon: {
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "baseline",
+  },
+});
